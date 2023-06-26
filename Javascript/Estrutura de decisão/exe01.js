344549742435783da730928fcd19e15d54c1a277
Ramon Albert <ramonalbert14@gmail.com>
2023-06-26 19:29:22 -0300

    modificado exemplo 1 linhas 5 e 8

--- Javascript/Estrutura de decisão/exe01.js
+++ Javascript/Estrutura de decisão/exe01.js
@@ -2,8 +2,8 @@ let num1 = parseFloat(prompt(`Digite o primeiro numero:`));
 let num2 = parseFloat(prompt(`Digite o segundo numero:`));
 
 if (num1 > num2){
-    alert(`O número ${num1} é maior.`);
+    alert(`O número ${num1} é bem maior.`);
 }
 else {
-    alert(`O número ${num2} é maior`);
+    alert(`O número ${num2} é muito maior`);
 };
