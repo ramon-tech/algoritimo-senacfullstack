538db9330f0e6d9cf8c7bfe923da8ecf1568e265
Ramon Albert <ramonalbert14@gmail.com>
2023-07-17 19:52:30 -0300

    orientacao a objetos

---
+++ Javascript/Orientacao-objetos/carro.js
@@ -0,0 +1,16 @@
+class Carro {
+
+    // atributos
+marca;
+modelo;
+ano;
+    // métodos
+
+
+}
+
+//instanciando o objeto up
+const up = new Carro();
+up.modelo = `up`;
+up.marca = `VW`;
+up.ano = 2015;
---
+++ Javascript/Orientacao-objetos/main.js
@@ -0,0 +1,7 @@
+import { Carro } from './carro';
+
+//instanciando o objeto up
+const up = new Carro(`Up`, `VW`, 2015);
+
+const ka = new Carro('Ka', 'Ford', 2006);
+
